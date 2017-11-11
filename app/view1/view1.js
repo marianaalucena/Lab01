7350dbacff7615290c5fb60714bb02c1d6b09ada
marianaalucena <mariana.lucena@ccc.ufcg.edu.br>
2017-11-11 05:51:37 -0200

    Até o caso de uso -5

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,57 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ["$scope", function($scope) {
+
+	$scope.artistas = [];
+	$scope.artista = "";
+	$scope.favoritos= [];
+
+	$scope.addArtista = function() {
+		if($scope.artista != '') {
+			for(var i=0; i<$scope.artistas.length; i++){
+				if($scope.artistas[i] == $scope.artista){
+					alert("Artista já existente.");
+					return;
+				}
+			}
+			$scope.artistas.push($scope.artista);
+			console.log($scope.artistas);
+			alert("Artista adicionado como sucesso.");
+		}
+		else{
+			alert("Artista inválido.");
+		}
+	}
+
+	$scope.buscarArtista = function() {
+		if($scope.artista != '') {
+			for(var i = 0; i < $scope.artistas.length; i++){
+				if($scope.artistas[i] == $scope.artista){
+					return $scope.artistas[i];
+				}
+			}
+		}		
+	}
+
+	$scope.addFavoritos = function() {
+		$scope.favoritos.push(buscarArtista());
+	}
+
+	$scope.haFavoritos = function(){
+		if($scope.favoritos.length > 0){
+			return true;
+		}
+		return false;
+	}
+
+	$scope.removeFavoritos = function(){
+		for(var i = 0; i < $scope.favoritos.length; i++){
+			if($scope.favoritos[i] == $scope.artista){
+				delete $scope.favoritos[i];
+			}
+		}
+	}	
+
 
 }]);
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,6 +9,69 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('View2Ctrl', ["$scope", function($scope) {
 
+	$scope.artistas = [];
+	$scope.artista = "";
+	$scope.album = [];
+	$scope.ano = "";
+	$scope.musica = [];
+	$scope.duracao = "";
+	$scope.img = "";
+
+
+
+	$scope.artistas = [
+		["O Rappa", [["Nunca tem fim", "2016", [["Cruz de tecido",	"4:05"]]]],
+		"img/rappa.jpg"],
+
+		["Nando Reis & os infernais", [["Luau MTV", "2007",	[["Relicário", "5:07"]]]],
+		"img/nando.jpg"]
+	];
+	$scope.informacoesCompletas = function(){
+		if($scope.artista != ''&& 
+			$scope.album != ''&&
+			$scope.musica != ''&& 
+			$scope.ano != ''&&
+			$scope.duracao != ''){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+	$scope.addMusica = function(){
+		for(var i = 0; i < $scope.artistas.length; i++){
+			if($scope.artistas[i][0] == $scope.artista){
+				for(var j = 0; j < $scope.artistas[i][1].length; j++){
+					if($scope.artistas[i][1][j][0] == $scope.album){
+						for(var k = 0; k < $scope.artistas[i][1][j][2].length; k++){
+							if($scope.artistas[i][1][j][2][k][0] == $scope.musica){
+								alert("Música já existente no álbum.")
+								return;
+							}
+							else{
+								$scope.artistas[i][1][j][2].push([$scope.musica, $scope.duracao]);
+								return;
+							}
+						}
+					}	
+					else{
+						$scope.artistas[i][1].push([$scope.album, $scope.ano, [[$scope.musica, $scope.duracao]]]);
+						return;
+					}
+				}
+			}	
+			else{
+				$scope.artistas.push([
+					$scope.artista,[[
+					$scope.album, $scope.ano, [[
+					$scope.musica, $scope.duracao]]]],
+					$scope.img
+					])
+				return;
+			}	
+	}
+}
 }]);
